c5fcb56f7f56ca2a61bef297d132e042340a225a
Nhu-Hoai Robert Vo <nhuhoai.vo@nhuvo.ch>
2020-10-08 22:21:42 +0000

    Add some PHP files for testing.

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Main call file
+ * 
+ * PHP Version 5.6, 7.2 and 7.4
+ * 
+ * @category  Test
+ * @package   TravisCiTest
+ * @author    Nhu-Hoai Robert Vo <nhuhoai.vo@nhuvo.ch>
+ * @copyright 2020 Nhu-Hoai Robert Vo
+ * @license   https://www.nhuvo.ch/Proprietary Proprietary project
+ * @version   GIT: 0.1.0
+ * @link      https://github.com/NhuVoCh/travis-ci-test
+ * @since     0.1.0
+ */
+
+print "Hello world!";
